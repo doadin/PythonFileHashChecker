82123cc5c848d158e3d3380809ce0ec0b267b829
Doadin <tbkizle@gmail.com>
2016-10-03 02:56:06 -0400

    Python 3 compat and isort

--- PythonFileHashChecker.py
+++ PythonFileHashChecker.py
@@ -1,6 +1,7 @@
-import hashlib
 import argparse
+import hashlib
 import os
+import sys
 
 parser = argparse.ArgumentParser(description='Check ISO MD5.')
 parser.add_argument("--file", help="File you want the hash for.")
@@ -12,10 +13,14 @@ selectedfile = args.file
 filename = os.path.basename(args.file)
 
 if args.hashtype:
-    if args.hashtype not in hashlib.algorithms:
-            raise NameError('The algorithm you specified is not supported')
+    if sys.version_info >= (3,0):
+        if args.hashtype not in hashlib.algorithms_available:
+                raise NameError('The algorithm you specified is not supported')
+    else:
+        if args.hashtype not in hashlib.algorithms:
+                raise NameError('The algorithm you specified is not supported')
 else:
-    print "--hashtype not supplied defaulting to md5"
+    print("--hashtype not supplied defaulting to md5")
     args.hashtype = "MD5"
 
 # Generate the selected files hash
@@ -42,20 +47,20 @@ if args.file and args.list:
     filenameinlist, correcthash = fileplushash[0],fileplushash[1]
     # Debug
     # print list[1]
-    print "the file"
-    print filename 
-    print "has the hash:"
-    print filehash
-    print "correct hash is:"
-    print correcthash
+    print("the file")
+    print(filename) 
+    print("has the hash:")
+    print(filehash)
+    print("correct hash is:")
+    print(correcthash)
     # Compair Hashes
     if filehash == correcthash:
-        print "Hashes match file is safe!"
+        print("Hashes match file is safe!")
     else:
-        print " Hashes DO NOT match file is corrupt!"
+        print(" Hashes DO NOT match file is corrupt!")
 else:
-    print "your file:"
-    print filename
-    print "has a hash of:"
-    print filehash
+    print("your file:")
+    print(filename)
+    print("has a hash of:")
+    print(filehash)
 
